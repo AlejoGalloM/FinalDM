5eb0ae1ce516bf850c766999766b965398462723
AlejoGalloM <galloalejo2@gmail.com>
2020-09-14 18:56:35 -0500

    Holi

---
+++ FourierAPP/app/src/main/java/com/uco/fourierapp/InsertarSenal.java
@@ -0,0 +1,14 @@
+package com.uco.fourierapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InsertarSenal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insertar_senal);
+    }
+}

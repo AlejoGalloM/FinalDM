f7cd8e2ede7a569a3ffa045f63790205af5e96bd
AlejoGalloM <galloalejo2@gmail.com>
2020-09-14 19:34:13 -0500

    cambios

---
+++ FourierAPP/app/src/main/java/com/uco/fourierapp/SenalGuardadas.java
@@ -0,0 +1,14 @@
+package com.uco.fourierapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SenalGuardadas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senal_guardadas);
+    }
+}
---
+++ FourierAPP/app/src/main/java/com/uco/fourierapp/SenalMov.java
@@ -0,0 +1,14 @@
+package com.uco.fourierapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SenalMov extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_senal_mov);
+    }
+}

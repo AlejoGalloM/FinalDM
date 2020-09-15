fd8ed3569b44f8146a7854c46b7778d31bed88a1
Alejo057 <42079935+Alejo057@users.noreply.github.com>
2020-09-14 22:35:45 -0500

    Enlaces entre las activities y solución de algunos errores con estas

--- FourierAPP/app/src/main/java/com/uco/fourierapp/InsertarSenal.java
+++ FourierAPP/app/src/main/java/com/uco/fourierapp/InsertarSenal.java
@@ -2,13 +2,25 @@ package com.uco.fourierapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class InsertarSenal extends AppCompatActivity {
+
+    private Button btnSenalSonido, btnSenalMovimiento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_insertar_senal);
+
+        btnSenalSonido = (Button) findViewById(R.id.senalSonido);
+        btnSenalMovimiento = (Button) findViewById(R.id.senalMovimiento);
+
+        btnSenalSonido.setOnClickListener(view ->
+                startActivity(new Intent(InsertarSenal.this, SenalSonido.class)));
+        btnSenalMovimiento.setOnClickListener(view ->
+                startActivity(new Intent(InsertarSenal.this, SenalMov.class)));
     }
 }
--- FourierAPP/app/src/main/java/com/uco/fourierapp/MainActivity.java
+++ FourierAPP/app/src/main/java/com/uco/fourierapp/MainActivity.java
@@ -2,16 +2,24 @@ package com.uco.fourierapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnInsertarSenal, btnVerSenalesGuardadas;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnInsertarSenal = (Button) findViewById(R.id.insertar);
+        btnVerSenalesGuardadas = (Button) findViewById(R.id.ver);
+
+        btnInsertarSenal.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, InsertarSenal.class)));
+        btnVerSenalesGuardadas.setOnClickListener(view ->
+                startActivity(new Intent(MainActivity.this, SenalGuardadas.class)));
     }
 }
